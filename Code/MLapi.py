a66259313d37bff5f049a3c1ddb34fcd2d4c4e76
Flo-Eh <116956039+Flo-Eh@users.noreply.github.com>
2023-05-12 22:41:39 +0200

    Update MLapi.py

--- Code/MLapi.py
+++ Code/MLapi.py
@@ -1,5 +1,5 @@
 import uvicorn 
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from pydantic import BaseModel
 import pickle 
 from sklearn.preprocessing import LabelEncoder
@@ -53,6 +53,23 @@ with open('SVR_model_best_param.sav','rb') as f:
 
 
 @app.post('/prediction/station')
+""" Predict emission for a choosen station\n
+Station list:['AP001', 'AP005', 'AS001', 'BR005', 'BR006', 'BR007', 'BR008',
+       'BR009', 'BR010', 'CH001', 'DL001', 'DL002', 'DL003', 'DL004',
+       'DL005', 'DL006', 'DL007', 'DL008', 'DL009', 'DL010', 'DL011',
+       'DL012', 'DL013', 'DL014', 'DL015', 'DL016', 'DL017', 'DL018',
+       'DL019', 'DL020', 'DL021', 'DL022', 'DL023', 'DL024', 'DL025',
+       'DL026', 'DL027', 'DL028', 'DL029', 'DL030', 'DL031', 'DL032',
+       'DL033', 'DL034', 'DL035', 'DL036', 'DL037', 'DL038', 'GJ001',
+       'HR011', 'HR012', 'HR013', 'HR014', 'JH001', 'KA002', 'KA003',
+       'KA004', 'KA005', 'KA006', 'KA007', 'KA008', 'KA009', 'KA010',
+       'KA011', 'KL002', 'KL004', 'KL007', 'KL008', 'MH005', 'MH006',
+       'MH007', 'MH008', 'MH009', 'MH010', 'MH011', 'MH012', 'MH013',
+       'MH014', 'ML001', 'MP001', 'MZ001', 'OD001', 'OD002', 'PB001',
+       'RJ004', 'RJ005', 'RJ006', 'TG001', 'TG002', 'TG003', 'TG004',
+       'TG005', 'TG006', 'TN001', 'TN002', 'TN003', 'TN004', 'TN005',
+       'UP012', 'UP013', 'UP014', 'UP015', 'UP016', 'WB007', 'WB008',
+       'WB009', 'WB010', 'WB011', 'WB012', 'WB013'] """
 def prediction(item:station):
     #open file, preprocess and split data
     df = pd.read_csv('station_hour.csv')
